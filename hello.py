bcc673ecf1f738328ecad76cfd0fd4edfea61565
Subhash <subhash@cnet-global.com>
2021-04-03 17:19:29 -0600

    Updated code

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/<name>')
 def helloWorld(name):
-    return f"<h1> Hello {name}!</h1>"
+    print(f'Hello, {name}!!!')
+
+
+helloWorld('Subhash')
